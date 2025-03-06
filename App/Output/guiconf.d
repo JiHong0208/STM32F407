ea3fe7ed4841b92537e85281cedb096f0a4e2b20
JiHong0208 <2581490240@qq.com>
2025-03-06 18:05:12 +0800

    移植了App_FreeRTOS版本，可以实现多任务调度。 版本3.0  2025.03.06  苏至锋

---
+++ App/Output/guiconf.d
@@ -0,0 +1,8 @@
+..\..\output\guiconf.o: ..\..\EMWin\Config\GUIConf.c
+..\..\output\guiconf.o: ..\..\EMWin\inc\GUI.h
+..\..\output\guiconf.o: ..\..\EMWin\inc\GUI_ConfDefaults.h
+..\..\output\guiconf.o: ..\..\EMWin\Config\GUIConf.h
+..\..\output\guiconf.o: ..\..\EMWin\inc\GUI_Type.h
+..\..\output\guiconf.o: ..\..\EMWin\inc\LCD.h
+..\..\output\guiconf.o: ..\..\EMWin\inc\Global.h
+..\..\output\guiconf.o: ..\..\EMWin\inc\GUI_Version.h
