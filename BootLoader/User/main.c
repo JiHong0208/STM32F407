a7e67e6fc9640cb16107a367d2e4f229513eae1b
JiHong0208 <2581490240@qq.com>
2025-03-16 22:06:53 +0800

    添加了Bootloader简单的屏幕引导，同时把Bootloader版本的代码格式和App_FreeRTOS版本统一。 版本4.02  2025.03.16  苏至锋

--- BootLoader/User/main.c
+++ BootLoader/User/main.c
@@ -1,10 +1,10 @@
 #include "stm32f4xx.h"
-#include "bsp_debug_usart.h"
+#include "fonts.h"
 #include "bsp_key.h"
 #include "bsp_led.h"
+#include "bsp_ili9341_lcd.h"
 #include "stm32f4xx_gpio.h"
-#include "stm32f4xx_usart.h"
-#include "stm32f4xx_rcc.h"
+#include "bsp_debug_usart.h"
 
 #define APP_ADDRESS 0x08020000
 
@@ -14,6 +14,7 @@ uint32_t JumpAddress;
 
 static void JumpToApp(void);
 static void BSP_Init(void);
+static void LCD_Start(void);
 static void Delay(__IO uint32_t nCount);
 
 /**
@@ -26,6 +27,9 @@ int main(void)
     // 初始化硬件
 	BSP_Init();
 
+	// 初始化LCD屏显示界面
+	LCD_Start();
+	
 	// 指示进入 Bootloader
     LED_BLUE;
 	printf("\r\n------------------进入到Bootloader程序------------------\r\n");
@@ -68,9 +72,31 @@ static void BSP_Init(void)
 {
 	// 初始化系统和硬件
     SystemInit();
+	ILI9341_Init();
     LED_GPIO_Config();
     Key_GPIO_Config();
     Debug_USART_Config();
+}
+
+static void LCD_Start(void)
+{
+	// 清屏，显示全黑背景
+	ILI9341_Clear(0,0,LCD_X_LENGTH,LCD_Y_LENGTH);
+	
+	// 设置字体颜色为白色，背景为黑色
+	LCD_SetColors(WHITE,BLACK);
+	
+	// 显示初始化信息
+	LCD_SetFont(&Font16x24);
+	ILI9341_DispStringLine_EN(LINE(1),"STM32F407_BMS");
+	
+	LCD_SetFont(&Font8x16);
+	ILI9341_DispStringLine_EN(LINE(5),"This is Bootloader.");
+	ILI9341_DispStringLine_EN(LINE(8),"Press Key1 jump to App.");
+	ILI9341_DispStringLine_EN(LINE(15),"           Author:ZhiFengSu");
+	ILI9341_DispStringLine_EN(LINE(16),"           Graduation:FJUT");
+
+	
 }
 
 static void Delay(__IO uint32_t nCount) {
